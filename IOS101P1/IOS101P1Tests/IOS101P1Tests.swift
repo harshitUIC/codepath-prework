751e509f65c6a9ec00a7486eff66de74974215f6
harshitUIC <hagga@uic.edu>
2025-08-25 19:07:36 -0500

    Added prework files to repo

---
+++ IOS101P1/IOS101P1Tests/IOS101P1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  IOS101P1Tests.swift
+//  IOS101P1Tests
+//
+//  Created by Harshit Aggarwal on 8/25/25.
+//
+
+import Testing
+@testable import IOS101P1
+
+struct IOS101P1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
